e6dc45a50a7b23e10884efc0248ed26f5381d655
Woody <colorguitar@hotmail.com>
2022-08-07 20:48:06 +0800

    first commit

---
+++ src/main/java/com/woody/woodycameraapi/WoodycameraApiApplication.java
@@ -0,0 +1,13 @@
+package com.woody.woodycameraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodycameraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodycameraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woody/woodycameraapi/WoodycameraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woody.woodycameraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodycameraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
